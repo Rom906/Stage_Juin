0bd4331ec7d6504c088a7fac990008330d933492
Romain <rgrezesmolenat@gmail.com>
2025-06-03 17:54:32 +0200

    Plein de tests de possiblitées qui ne marche pas

--- debut.py
+++ debut.py
@@ -1,37 +1,8 @@
-from fonctions import generation_pdf, ecrire_latex, generer_debut_latex_amc, combiner
+from fonctions import compiler_amc, ecrire_latex, generer_debut_latex_amc
 
 debut = generer_debut_latex_amc("Test 1")
-corps = r"""\documentclass[a4paper,12pt]{exam}
 
-\usepackage[utf8]{inputenc}
-\usepackage[T1]{fontenc}
-\usepackage[french]{babel}
 
-\begin{document}
+fichier_latex = ecrire_latex(debut, "qcm_exemple.tex")
 
-\begin{questions}
-
-% Question ouverte
-\question[5] Expliquez pourquoi Paris est la capitale de la France.
-
-% Question QCM (plusieurs choix, une seule bonne réponse)
-\question[3] Quelle est la capitale de la France ?
-
-\begin{choices}
-    \choice Lyon
-    \CorrectChoice Paris
-    \choice Marseille
-\end{choices}
-
-% Question à réponse courte (l’élève doit écrire un mot ou une phrase courte)
-\question[2] Donnez la couleur du drapeau français.
-
-\end{questions}
-
-\end{document}
-"""
-total = combiner(debut, corps)
-
-fichier_latex = ecrire_latex(corps, "qcm_exemple.tex")
-
-generation_pdf(fichier_latex)
+compiler_amc(fichier_latex)
--- fonctions.py
+++ fonctions.py
@@ -1,17 +1,16 @@
 import subprocess
-
+import os
 
 def generer_debut_latex_amc(titre):
     debut = r"""\documentclass[a4paper]{article}
 \usepackage[utf8]{inputenc}
 \usepackage[T1]{fontenc}
 \usepackage[french]{babel}
-\usepackage{automultiplechoice}
+\usepackage[noautomark]{automultiplechoice}
 
 \begin{document}
-\onecopy{1}{
-\AMCtitle{%s}
-""" % titre
+
+\onecopy[draft=false]{1}{"""
     return debut
 
 
@@ -21,10 +20,31 @@ def combiner(fichier1, fichier2):
 
 def ecrire_latex(fichier_texte, titre):
     with open(titre, "w", encoding="utf-8") as fichier:
-        fichier.write(fichier_texte + r"""\end{document}""")
+        fichier.write(fichier_texte + r"""}
+    \end{document}""")
     return titre
 
 
 def generation_pdf(fichier_latex):
-    pdf = subprocess.run(["pdflatex", fichier_latex])
-    return pdf
+     pdf = subprocess.run(["pdflatex", fichier_latex])
+     return pdf
+
+
+def ajouter(nombre):
+
+# def compiler_amc(nom_fichier_tex: str):
+#     prefix = os.path.splitext(nom_fichier_tex)[0]
+#     try:
+#         subprocess.run([
+#             "auto-multiple-choice", "prepare",
+#             "--mode", "s",
+#             "--prefix", ".",
+#             nom_fichier_tex
+#         ], check=True)
+
+#         subprocess.run([
+#             "auto-multiple-choice", "meptex",
+#             "--src", nom_fichier_tex,
+#             "--out", f"{prefix}-sujet.pdf",
+#             "--prefix", "."
+#         ], check=True)
