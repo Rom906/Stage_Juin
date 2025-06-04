09bcb8e65bedde4f6039e694891bf939abbee029
Romain <rgrezesmolenat@gmail.com>
2025-06-04 16:51:50 +0200

    Nouvelles fonctions géénrations du qcm

--- base_donnee.py
+++ base_donnee.py
@@ -1,7 +1,7 @@
 import yaml
+import subprocess
 
 
-# Fonction de transformation (identique à celle de tout à l’heure)
 def question_to_latex(q):
     latex = "\\vbox{\n"
     latex += "    \\section{" + q.get("section", "") + "}\n"
@@ -19,9 +19,82 @@ def question_to_latex(q):
     return latex
 
 
-with open("questions.yaml", "r", encoding="utf-8") as f:
+def combiner(fichier1, fichier2):
+    return fichier1 + fichier2
+
+
+def generation_pdf(fichier_latex):
+    preambule = r"""\documentclass[12pt]{article}
+\usepackage[utf8]{inputenc}
+\usepackage{graphicx}
+\usepackage{enumitem,amssymb}
+\usepackage{tabularx}
+\usepackage{calc}
+\usepackage{cprotect}
+\usepackage{xcolor}
+\usepackage[a4paper,textwidth=16cm,top=2cm,bottom=2cm,headheight=25pt,headsep=12pt,footskip=25pt]{geometry}
+\usepackage{fancybox}
+\newenvironment{framed}%
+  {\begin{Sbox}\begin{minipage}{\dimexpr\linewidth-2\fboxrule-2\fboxsep}}%
+  {\end{minipage}\end{Sbox}\fbox{\TheSbox}}
+
+\usepackage{pifont}
+\usepackage{ifthen}
+
+\newcommand{\corrige}{0}
+\newcounter{possibility}
+
+\newcommand{\correct}{%
+  \addtocounter{possibility}{1}
+  \ifthenelse{\equal{\corrige}{0}}%
+    {\item[\ding{\the\numexpr\value{possibility}}]}%
+    {\item[\textcolor{red}{\ding{\the\numexpr\value{possibility}-10}}]}%
+}
+
+\newcommand{\leurre}{%
+  \addtocounter{possibility}{1}
+  \item[\ding{\the\numexpr\value{possibility}}]%
+}
+
+\newcommand{\enonce}[1]{%
+  \noindent
+  Question~:
+  \vspace*{.5\baselineskip}
+
+  \noindent
+  \begin{framed}
+    #1
+  \end{framed}
+}
+
+\newcommand{\difficulte}[1]{%
+  Difficulté : 
+  \ifthenelse{#1 > 1 \or #1 = 1}{\ding{72}}{\ding{73}} 
+  \ifthenelse{#1 > 2 \or #1 = 2}{\ding{72}}{\ding{73}}
+  \ifthenelse{#1 > 3 \or #1 = 3}{\ding{72}}{\ding{73}}
+  \ifthenelse{#1 > 3}{S}{}%
+}
+
+\newcommand{\possibilites}[1]{%
+  \vspace*{\baselineskip}
+  \begin{itemize}[label={}]
+    #1
+  \end{itemize}
+}
+
+\begin{document}
+"""
+    latex = combiner(preambule, fichier_latex)
+    pdf = subprocess.run(["pdflatex", latex + r"""\end{document}"""])
+    return pdf
+
+
+latex_code = ""
+with open("qcm_questions.yaml", "r", encoding="utf-8") as f:
     base_de_donnée = yaml.safe_load(f)
 
 for question in base_de_donnée["questions"]:
-    latex_code = question_to_latex(question)
-    print(latex_code) 
+    latex_code += question_to_latex(question)
+
+
+generation_pdf(latex_code)
