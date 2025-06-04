f7d1c8284c0e5f3dd043d17d7079cd9bf27b7b9f
Romain <rgrezesmolenat@gmail.com>
2025-06-04 23:16:22 +0200

    Ajout random

--- base_donnee.py
+++ base_donnee.py
@@ -1,6 +1,7 @@
 import yaml
 import subprocess
-
+import random
+import shuffle
 
 def question_to_latex(q):
     latex = "\\section{" + q.get("section", "") + "}\n"
@@ -79,6 +80,50 @@ def ecrire_latex(contenu_questions, nom_fichier):
 \newcommand{\pourquoi}[1]{}
 
 \begin{document}
+\begin{center}
+  \begin{tabular}{c}
+  \hline\\%\vspace{0.1cm}
+  {\textsc{\'Ecole Centrale Marseille}}\vspace{0.1cm}
+  \\
+
+    {\bf {\Large Modélisation et Conception Objet}}\\%\vspace{0.2cm}
+    \\
+    {\bf  { Contrôle 1A }}\\
+    {\footnotesize 27/06/2107}\\
+    \hline
+  \end{tabular}
+\end{center}
+\vspace{0.6cm}
+
+\noindent
+{\em Les réponses sont à donner directement sur le sujet. Un espace est réservé pour chaque réponse.}
+
+\vspace*{1cm}
+\noindent
+\begin{tabular}{|l|p{10cm}|}
+    \hline
+    Nom : & \\
+    \hline
+    Prénom : & \\
+    \hline
+    
+\end{tabular}
+
+
+\vspace*{2cm}
+\noindent
+Barème. Pour chaque question : 
+\begin{itemize}
+    \item ne pas répondre donne 0 point,
+    \item répondre de façon exacte donne : $\mathbf{+\frac{2}{3}}$ points
+    \item répondre de façon inexacte\footnote{le nombre de points négatifs varie pour que si l'on répond de façon aléatoire l'espérance soit nulle} : 
+    \begin{itemize}
+        \item pour une réponse de type VRAI/FAUX : $\mathbf{-\frac{2}{3}}$ points,
+        \item pour une réponse libre : $\mathbf{-\frac{2}{3}}$ points,
+        \item pour une réponse de type 1 parmi 3 : $\mathbf{-\frac{1}{3}}$ points.
+    \end{itemize}
+\end{itemize}
+\newpage
 """
     with open(nom_fichier, "w", encoding="utf-8") as fichier:
         fichier.write(preambule)
@@ -92,13 +137,30 @@ def generation_pdf(nom_fichier):
     return pdf
 
 
-with open("qcm_questions.yaml", "r", encoding="utf-8") as fichier:
-    base = yaml.safe_load(fichier)
+def generate_exam(nombre_question: int, theme: str):
+    with open("qcm_questions.yaml", "r", encoding="utf-8") as fichier:
+        base = yaml.safe_load(fichier)
 
-latex_code = ""
-for question in base["questions"]:
-    latex_code += question_to_latex(question) + "\n"
+    latex_code = ""
+    compteur = 0
+    for question in base[theme]:
+        compteur +=1
 
-ecrire_latex(latex_code, "prime.tex")
-
-generation_pdf("prime.tex")
+    for _ in range(compteur):
+        Liste = []
+        hasard = random.randint(0, compteur)
+        if compteur < nombre_question:
+            for i in range(compteur):
+                Liste.append(base[theme[i]])
+        else:
+            if base[theme[hasard]] in Liste:
+                while base[theme[hasard]] in Liste:
+                    hasard = random.randint(0, compteur)
+            else:
+                Liste.append(base[theme[hasard]])
+    random.shuffle(Liste)
+    for i in range(len(Liste)):
+        latex_code += question_to_latex(question) + "\n"
+    ecrire_latex(latex_code, "prime.tex")
+    final = generation_pdf("prime.tex")
+    return final
