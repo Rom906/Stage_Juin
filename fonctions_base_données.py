48613676925f89165d98781ddde337ed1afee86c
Romain <rgrezesmolenat@gmail.com>
2025-06-23 12:54:34 +0200

    Suppression code inutile

--- fonctions_base_données.py
+++ fonctions_base_données.py
@@ -2,26 +2,6 @@ import yaml
 import os
 
 
-def combiner(base1, base2, sortie):
-    with open(base1, "r", encoding="utf-8") as f1:
-        base = yaml.safe_load(f1)
-    with open(base2, "r", encoding="utf-8") as f2:
-        base2_data = yaml.safe_load(f2)
-    if type(base) == list and type(base2_data) == list:
-        resultat = base + base2_data
-    elif type(base) == dict and type(base2_data) == dict:
-        resultat = base.copy()
-        resultat.update(base2_data)
-    else:
-        resultat = [base, base2_data]
-    with open(sortie, "w", encoding="utf-8") as f_out:
-        resultat_final = yaml.dump(resultat, f_out)
-    return resultat_final
-
-
-# combiner("test.yaml", "test2.yaml", "resultat2.yaml"
-
-
 def charger_questions(fichier_yaml):
     dossier = os.path.dirname(fichier_yaml)
     with open(fichier_yaml, "r", encoding="utf-8") as fichier:
@@ -65,7 +45,8 @@ def filtre_exercices(exercices, tags_recherches, mode="AND"):
     - mode "OR" : au moins un tag doit être présent
     - mode "AND_SPECIAL" :
          soit tous les tags sont présents,
-         soit l'exercice a exactement un tag parmi les tags recherchés (aucun autre tag)
+         soit l'exercice a exactement un tag parmi les tags recherchés
+         (aucun autre tag)
     """
     exercices_filtres = []
 
